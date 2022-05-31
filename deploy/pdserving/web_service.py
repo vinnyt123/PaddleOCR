72c596f00c6ba6da2eb451fe601d92a578a179cd
Vinny Tunnell <vinny.tunnell@plexure.com>
2022-05-31 22:32:31 +1200

    print input dicts

--- deploy/pdserving/web_service.py
+++ deploy/pdserving/web_service.py
@@ -84,8 +84,12 @@ class RecOp(Op):
         self.sorted_boxes = SortedBoxes()
 
     def preprocess(self, input_dicts, data_id, log_id):
+        print("printing input dict")
+        print(input_dicts)
         ((_, input_dict),) = input_dicts.items()
         raw_im = input_dict["image"]
+        print("xxx printing input dictuuuu")
+        print(input_dict)
         data = np.frombuffer(raw_im, np.uint8)
         im = cv2.imdecode(data, cv2.IMREAD_COLOR)
         self.dt_list = input_dict["dt_boxes"]
