25f822d03cff5b883da7072f906d68a8c2b1e38d
Vinny Tunnell <vinny.tunnell@plexure.com>
2022-07-02 10:12:37 +1200

    Comment out mapper for test

--- deploy/pdserving/mapper.py
+++ deploy/pdserving/mapper.py
@@ -3,7 +3,7 @@ def map_res_to_selectext_format(res):
     
     out = []
     for el in res:
-        word = el[0]
+        word, confidence = el[0]
         bounding_box_initial = el[1]
         bounding_box = [{"x": bound[0], "y": bound[1]} for bound in bounding_box_initial]
         out.append({
