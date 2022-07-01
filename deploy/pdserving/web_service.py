eb12373023b7fc2d7ee539b21223d19bc3804ccc
Vinny Tunnell <vinny.tunnell@plexure.com>
2022-07-02 09:56:44 +1200

    convert res to string before map

--- deploy/pdserving/web_service.py
+++ deploy/pdserving/web_service.py
@@ -174,7 +174,7 @@ class RecOp(Op):
             dt_box = self.dt_list[i]
             if text[1] >= 0.5:
                 result_list.append([text, dt_box.tolist()])
-        res = {"result": json.dumps(map_res_to_selectext_format(result_list))}
+        res = {"result": json.dumps(map_res_to_selectext_format(str(result_list)))}
         return res, None, ""
 
 
